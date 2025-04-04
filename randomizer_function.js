44ecce2f9fc486058c04fc350129badefa4d577f
Victor B <145293037+VictorBruson@users.noreply.github.com>
2025-04-04 17:55:14 -0400

    view sql

---
+++ randomizer_function.js
@@ -0,0 +1,54 @@
+
+const fs = require('fs');
+const path = require('path');
+
+function getRandomNameByDate(date, sex) {
+    try {
+        const year = date.split('-')[0].trim();
+
+        const fileName = `yob${year}${sex}`;
+        const filePath = path.join(__dirname,"/data/names/", fileName);
+
+        if (!fs.existsSync(filePath)) {
+            throw new Error(`No name data found for year ${year}` + filePath);
+        }
+
+
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+
+        // Split into array of names (assuming one name per line)
+        const names = fileContent.split('\n').filter(name => name.trim() !== '');
+
+        if (names.length === 0) {
+            throw new Error(`No names found in the file for year ${year}`);
+        }
+
+        // Select a random name from the array
+        const randomIndex = Math.floor(Math.random() * names.length);
+        console.log(names[randomIndex]);
+        return names[randomIndex];
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        return null;
+    }
+}
+
+function getRandomSurname(){
+    try{
+        const filePath = path.join(__dirname,"/data/surname.txt");
+        const fileContent = fs.readFileSync(filePath, 'utf8');
+
+        const surnames = fileContent.split('\n');
+
+        return surnames[Math.floor(Math.random() * surnames.length)].slice(0,-2);
+    }
+    catch (error){
+        console.error(`Error: ${error.message}`);
+        return null;
+    }
+
+
+}
+
+
+
